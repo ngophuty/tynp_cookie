2caeffb17ed702e6c70081ff14c3929858267170
ngophuty <ngoputy233@gmail.com>
2022-10-11 14:31:06 +0700

    ư

---
+++ tynp/main.py
@@ -0,0 +1,20 @@
+from env import env
+
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/',tags=['Hello'])
+async def hello_world():
+    return {'message':'hello world!'}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        'main:app',
+        host= env.HOST,
+        port= env.PORT,
+        reload= True
+    )
