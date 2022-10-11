d3ad879ec91bbd5598261b15e1f30da4a3eb42ba
ngophuty <ngoputy233@gmail.com>
2022-10-11 15:10:53 +0700

    s

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/database/connect_database.py
@@ -0,0 +1,13 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from {{cookiecutter.file_env_name}} import env 
+
+
+MONGO_DETAIL = f'{env.DATABASE_NAME}://{env.DATABASE_HOST}:{env.DATABASE_PORT}'
+client = AsyncIOMotorClient(MONGO_DETAIL)
+create_database = client.example
+collection = create_database.get_collection('exaple_mongodb')
+
+async def example():
+    data = {'example':'connect to mongodb'}
+    await collection.insert_one(data)
+    return
